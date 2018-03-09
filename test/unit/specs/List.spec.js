2f6fe48b48f6f49fe0dbf755b35690c8fd454894
oluwafemi medalem <oluwafemi.medale@andela.com>
2018-03-09 16:43:27 +0100

    add list component test

---
+++ test/unit/specs/List.spec.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import List from "@/components/List";
+
+describe("List.vue", () => {
+  it("displays items from the list", () => {
+    // our test goes here
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+    expect(ListComponent.$el.textContent).to.contain("play games");
+  });
+
+  it("adds a new item to list on click", () => {
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+
+    ListComponent.newItem = "brush my teeth";
+
+    const button = ListComponent.$el.querySelector("button");
+    const clickEvent = new window.Event("click");
+    button.dispatchEvent(clickEvent);
+    ListComponent._watcher.run();
+
+    expect(ListComponent.$el.textContent).to.contain('brush my teeth');
+    expect(ListComponent.listItems).to.contain('brush my teeth');
+  });
+});
